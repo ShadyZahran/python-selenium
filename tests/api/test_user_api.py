ca693e1ffe0bedd686fe32385d13aea871916b17
Shady Zahran <zahran.shady@gmail.com>
2025-02-14 20:08:00 +0100

    tests: extended api login tests
    Added test to validate the json response
    Refactored original test to check access token and token type

--- tests/api/test_user_api.py
+++ tests/api/test_user_api.py
@@ -17,6 +17,29 @@ logger = logging.getLogger(__name__)
 def test_customer_login_valid_credentials(
     email: str, password: str, practice_backend_api: PracticeBackendAPI
 ) -> None:
-    """Test user login endpoint using valid customer credentials"""
+    """Test user login endpoint returns bearer access token using valid customer credentials"""
+
     response = practice_backend_api.user_controller.login(email, password)
+
     assert response.status_code == HTTPStatus.OK
+    assert response.json()["access_token"]
+    assert response.json()["token_type"] == "bearer"
+
+
+@allure.feature("Login")
+@allure.story("API: Login with valid credentials")
+@pytest.mark.parametrize(
+    "email, password", [("customer@practicesoftwaretesting.com", "welcome01")]
+)
+def test_customer_login_valid_response(
+    email: str, password: str, practice_backend_api: PracticeBackendAPI
+) -> None:
+    """Test user login endpoint has valid response using valid customer credentials"""
+
+    expected_keys = ["access_token", "token_type", "expires_in"]
+
+    response = practice_backend_api.user_controller.login(email, password)
+    missing_keys = [key for key in expected_keys if key not in response.json()]
+
+    assert response.status_code == HTTPStatus.OK
+    assert not missing_keys, f"{missing_keys} not found in response"
